5e5d2525306967e849c2a0435f4021d3ba46139b
Robotxm <wanglinpei65@gmail.com>
2026-04-18 12:58:55 +0800

    fix: undefined 'lyricsMetadata' in netease searcher

--- current/yrc/searcher/netease_ex.js
+++ current/yrc/searcher/netease_ex.js
@@ -2,7 +2,7 @@
  * NetEase Cloud Music Lyrics Searcher Ex
  * Original Author: ohyeah
  * Modified by: Robotxm
- * Version: 0.2
+ * Version: 0.3
  * License: GPL 3.0
  * Description: Make foobar2000 with ESLyric able to parse lyrics provided by NetEase Cloud Music.
  * Github: https://github.com/Robotxm/ESLyric-LyricsSource
@@ -16,19 +16,19 @@ import crypto from 'crypto-es/lib/index.js'
 
 evalLib('querystring/querystring.min.js')
 
-const linuxApiIv = crypto.enc.Latin1.parse('0102030405060708')
-const linuxApiKey = crypto.enc.Latin1.parse('rFgB&h#%2?^eDg:Q')
-const eApiKey = crypto.enc.Latin1.parse('e82ckenh8dichen8')
+const iv = crypto.enc.Latin1.parse('0102030405060708')
+const linuxapiKey = crypto.enc.Latin1.parse('rFgB&h#%2?^eDg:Q')
+const anonymousToken = "bf8bfeabb1aa84f9c8c3906c04a04fb864322804c83f5d607e91a04eae463c9436bd1a17ec353cf780b396507a3f7464e8a60f4bbc019437993166e004087dd32d1490298caf655c2353e58daa0bc13cc7d5c198250968580b12c1b8817e3f5c807e650dd04abd3fb8130b7ae43fcc5b"
 
 const aesEncrypt = (buffer, mode, key, iv) => {
     const cipher = crypto.AES.encrypt(buffer, key, { mode: mode, iv: iv })
     return cipher.ciphertext
 }
 
-const linuxApi = (object) => {
+const linuxapi = (object) => {
     const text = JSON.stringify(object)
     return {
-        eparams: aesEncrypt(crypto.enc.Utf8.parse(text), crypto.mode.ECB, linuxApiKey, linuxApiIv).toString(crypto.enc.Hex).toUpperCase()
+        eparams: aesEncrypt(crypto.enc.Utf8.parse(text), crypto.mode.ECB, linuxapiKey, iv).toString(crypto.enc.Hex).toUpperCase()
     }
 }
 
@@ -50,42 +50,14 @@ const doRequest = (method, url, data, options) => {
         if (url.includes('music.163.com'))
             headers['Referer'] = 'https://music.163.com'
         if (options.crypto === 'linuxapi') {
-            data = linuxApi({
+            data = linuxapi({
                 method: method,
                 url: url.replace(/\w*api/, 'api'),
                 params: data
             })
             headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+            headers['Cookie'] = `MUSIC_A=${anonymousToken}`
             url = 'https://music.163.com/api/linux/forward'
-        } else if (options.crypto === 'eapi') {
-            const cookie = options.cookie || {}
-            const csrfToken = cookie['__csrf'] || ''
-            const header = {
-                osver: cookie.osver,
-                deviceId: cookie.deviceId,
-                appver: cookie.appver || '8.7.01',
-                versioncode: cookie.versioncode || '140',
-                mobilename: cookie.mobilename,
-                buildver: cookie.buildver || Date.now().toString().substr(0, 10),
-                resolution: cookie.resolution || '1920x1080',
-                __csrf: csrfToken,
-                os: cookie.os || 'android',
-                channel: cookie.channel,
-                requestId: `${Date.now()}_${Math.floor(Math.random() * 1000)
-                    .toString()
-                    .padStart(4, '0')}`,
-            }
-            if (cookie.MUSIC_U) header['MUSIC_U'] = cookie.MUSIC_U
-            if (cookie.MUSIC_A) header['MUSIC_A'] = cookie.MUSIC_A
-            headers['Cookie'] = Object.keys(header)
-                .map(
-                    (key) =>
-                        encodeURIComponent(key) + '=' + encodeURIComponent(header[key]),
-                )
-                .join('; ')
-            data.header = header
-            data = eApi(options.url, data)
-            url = url.replace(/\w*api/, 'eapi')
         } else {
             reject()
             return
@@ -118,7 +90,7 @@ const procKeywords = (str) => {
 
 export function getConfig(config) {
     config.name = "网易云音乐 Ex"
-    config.version = "0.2"
+    config.version = "0.3"
     config.author = "Robotxm"
 }
 
@@ -137,29 +109,15 @@ export function getLyrics(meta, man) {
         data,
         { crypto: 'linuxapi' }
     ).then((body) => {
-        let candicates = []
-        candicates = parseSearchResults(body)
+        let candicates = parseSearchResults(body)
         for (const item of candicates) {
             const queryData = {
-                id: item.id,
-                cp: false,
-                tv: 0,
-                lv: 0,
-                rv: 0,
-                kv: 0,
-                yv: 0,
-                ytv: 0,
-                yrv: 0,
-            }
-            // Mobile API whose response may contain yrc (an advanced lyrics format like krc)
-            doRequest(
-                'POST',
-                'https://interface3.music.163.com/eapi/song/lyric/v1',
+                id: item.id
+            };
+            doRequest('POST',
+                'https://music.163.com/weapi/song/lyric?lv=-1&kv=-1&tv=-1',
                 queryData,
-                {
-                    crypto: 'eapi',
-                    url: '/api/song/lyric/v1'
-                }
+                { crypto: 'linuxapi' }
             ).then((body) => {
                 parseLyricResponse(item, man, body)
             })
@@ -176,8 +134,9 @@ function parseSearchResults(body) {
         let results = obj['result'] || {}
         let songs = results['songs'] || []
         for (const song of songs) {
-            if (typeof (song['id']) === 'undefined' || typeof (song['name']) === 'undefined')
+            if (typeof (song['id']) === 'undefined' || typeof (song['name']) === 'undefined') {
                 continue
+            }
             let id = song['id']
             let title = song['name']
             let artist = ''
@@ -192,16 +151,14 @@ function parseSearchResults(body) {
             album = album['name'] || ''
             candicates.push({ id: id, title: title, artist: artist, album: album })
         }
-    } catch (e) {
-        console.log(e)
-    }
-
+    } catch (e) { }
     return candicates
 }
 
 function parseLyricResponse(item, man, body) {
     try {
         let lyricObj = JSON.parse(body)
+        let lyricText = ''
 
         const lyricsMetadata = man.createLyric()
 
@@ -218,7 +175,9 @@ function parseLyricResponse(item, man, body) {
                 lyricText = lyricObj['lrc']['lyric']?.replace(/^\{(.*)\}\s*/mg, "")?.trim() ?? ''
 
                 let version = lyricObj['lrc']['version'] || 0
-                if (version == 1) return
+                if (version == 1) {
+                    return
+                }
             }
 
             // Append translation if exists
@@ -232,7 +191,7 @@ function parseLyricResponse(item, man, body) {
             lyricsMetadata.lyricText = lyricText
             man.addLyric(lyricsMetadata)
         }
-    } catch (e) {
+    } catch (e) { 
         console.log(e)
     }
 }
